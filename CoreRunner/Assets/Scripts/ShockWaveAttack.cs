1f150730b7d1a2d09c10fbdaadea2bffd43d4a90
Emma Gillespie <emma7gillespie@gmail.com>
2020-02-21 14:17:50 -0700

    RobotA has ShockWave attack

--- CoreRunner/Assets/Scripts/ShockWaveAttack.cs
+++ CoreRunner/Assets/Scripts/ShockWaveAttack.cs
@@ -2,18 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour
+public class ShockWaveAttack : MonoBehaviour
 {
     public GameObject Hitbox;
     public float Timer;
     public bool AttackingIsRunning = false;
+    public bool CanAttack;
+    public float AttackCycleTimer;
 
     void OnTriggerEnter()
     {
             if(AttackingIsRunning == false)
             {
+                CanAttack = true; 
                 StartCoroutine(Attacking());
-            }      
+            }     
+    }
+
+    void OnTriggerExit()
+    {
+        CanAttack = false;
     }
 
     IEnumerator Attacking()
@@ -21,14 +29,23 @@ public class EnemyAttack : MonoBehaviour
         AttackingIsRunning = true;
 
         //turn on hitbox
-        Hitbox.SetActive(true);
+        Hitbox.GetComponent<Animator>().SetBool("Attack", true);
         
         //Set timer
         yield return new WaitForSeconds(Timer); 
 
         //turn off hitbox
-        Hitbox.SetActive(false);
+         Hitbox.GetComponent<Animator>().SetBool("Attack", false);
 
         AttackingIsRunning = false;
+    }
+
+    IEnumerator AttackCycle()
+    {
+        while(CanAttack == true)
+        {
+            StartCoroutine(Attacking());
+            yield return new WaitForSeconds(AttackCycleTimer);
+        }
     }
 }
