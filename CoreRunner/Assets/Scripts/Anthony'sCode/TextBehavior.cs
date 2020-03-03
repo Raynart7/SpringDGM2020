d1f78e55c933561f76befc8dc7e16a282a944b25
Emma Gillespie <emma7gillespie@gmail.com>
2020-03-03 13:17:20 -0700

    Anthony: hulk ref Core Runner: dialog unity event added Rodayne: animatic started

---
+++ CoreRunner/Assets/Scripts/Anthony'sCode/TextBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+    
+    
+[RequireComponent(typeof(Text))]
+public class TextBehavior : MonoBehaviour
+{
+    private Text textObj;
+    public StringListData stringListDataObj;
+
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        textObj.text = stringListDataObj.ReturnCurrentLine();
+    }
+
+}
